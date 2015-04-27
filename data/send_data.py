37e4d112eb4b5260637f66c6ba0c7d832d03198c
chrischris292 <cbchan2@illinois.edu>
2015-04-27 13:20:26 -0500

    done

--- data/send_data.py
+++ data/send_data.py
@@ -1,3 +1,3 @@
 import predictionio
 engine_client = predictionio.EngineClient(url="http://localhost:8000")
-print engine_client.send_query({"sentence": "Want your skin feeling softer, fuller and more youthful"})
+print engine_client.send_query({"sentence": "Visit Us Today  Genuine Gemstone Birthstone Rings $26"})
